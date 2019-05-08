5497f400eb8e6bb1e71e122814e58bc35b0efea8
spinon187 <spinon187@gmail.com>
2019-05-08 15:47:28 -0500

    max heap insert, get size work

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,27 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    if (len(self.storage) != 0):
+      return self.storage[0]
 
   def get_size(self):
     pass
 
-  def _bubble_up(self, index):
-    pass
+  def _bubble_up(self, i):
+    while i > 0:
+      parent = (i - 1) // 2
+      if self.storage[i] > self.storage[parent]:
+        self.storage[i], self.storage[parent] = self.storage[parent], self.storage[i]
+        i = parent
+      else:
+        break
 
   def _sift_down(self, index):
     pass
